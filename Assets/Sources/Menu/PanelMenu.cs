36e0abcaa78fb442006f58fa61b000f02ae0914b
Roman784 <rmironenko50@gmail.com>
2023-11-19 23:49:38 +0300

    Переработка меню паузы.

---
+++ Assets/Sources/Menu/PanelMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMenu : Menu
+{
+    [SerializeField] private GameObject _panel;
+
+    public void OpenPanel()
+    {
+        _panel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        _panel.SetActive(false);
+    }
+}
--- Assets/Sources/Menu/PauseMenu.cs
+++ Assets/Sources/Menu/PauseMenu.cs
@@ -2,23 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelMenu : Menu
+public class PauseMenu : PanelMenu
 {
-    [SerializeField] private GameObject _panel;
-
-    [Space]
-
     [SerializeField] private string _mainMenuName;
-
-    public void OpenMenuPanel()
-    {
-        _panel.SetActive(true);
-    }
-
-    public void CloseMenuPanel()
-    {
-        _panel.SetActive(false);
-    }
 
     public void OpenMainMenu()
     {
